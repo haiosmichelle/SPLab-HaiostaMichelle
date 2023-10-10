cd427625f332a44b08179cf5627bd02db134bbfc
haiosmichelle <94860504+haiosmichelle@users.noreply.github.com>
2023-10-10 08:44:23 +0300

    first commit

---
+++ src/main/java/ro/uvt/info/designpatternslab2023/DesignPatternsLab2023Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatternslab2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsLab2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsLab2023Application.class, args);
+    }
+
+}
