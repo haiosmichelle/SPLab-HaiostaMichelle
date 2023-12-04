6640cb3b3c89caa504b9b051a309042f011c109e
haiosmichelle <94860504+haiosmichelle@users.noreply.github.com>
2023-12-04 15:49:18 +0200

    lab_9

--- src/main/java/ro/uvt/info/designpatternslab2023/services/AlignRight.java
+++ src/main/java/ro/uvt/info/designpatternslab2023/services/AlignRight.java
@@ -7,7 +7,7 @@ public class AlignRight implements AlignStrategy{
     public void render(String p, Context c) {
         System.out.println("Aligning paragraph to the right:");
         for (int i = 0; i < c.getNumber_row(); i++) {
-            for (int j = 0; j < c.getNumber_coll() - p.length(); j++) {
+            for (int j = 0; j < c.getNumber_coll() - p .length(); j++) {
                 System.out.print(" ");
             }
             System.out.println(p);
